e7092e789407589c5916080f0b60ca111b4e922b
Lukar10Y <lukariXU01@gmail.com>
2023-05-13 22:48:37 -0400

    Codigo de Luis. XAML Agregar,Editar

--- Views/AgregarEditarContacto.xaml.cs
+++ Views/AgregarEditarContacto.xaml.cs
@@ -20,11 +20,9 @@ public partial class AgregarEditarContacto : ContentPage
 			if (Convert.ToInt32(value) != -1)
 			{
                 _contacto = Funciones.BuscarContacto(Convert.ToInt32(value));
-                LabelDePrueba.Text = $"Nombre: {_contacto.Nombre},  Apellido: {_contacto.Apellido}";
             }
 			else
 			{
-				LabelDePrueba.Text = "Crear Contacto";
 			}
 		}
 	}
